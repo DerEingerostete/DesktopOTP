3e71347c8b574a3c4f1139359d487b4bf3cbd438
DerEingerostete <git@dereingerostete.dev>
2023-06-16 14:34:07 +0200

    Replaced placeholder code with working code

--- src/components/token/TokenComponent.vue
+++ src/components/token/TokenComponent.vue
@@ -2,41 +2,103 @@
 import {Token} from "../../assets/js/token/Token";
 import {ref} from "vue";
 import {ProfileGenerator} from "../../assets/js/ProfileGenerator";
+import {GeneratorsCallback, TokenGenerator, TokenGenerators} from "../../assets/js/token/TokenGenerator";
+import {Dialog} from "quasar";
 
 const props = defineProps<{
     tokenEntry: Token
 }>();
 
+const generator: TokenGenerator = new TokenGenerator(props.tokenEntry);
+const tokenText = ref("");
+
+//References
+const displayNames: string[] = props.tokenEntry.getDisplayNames();
 const imageSrc = ref(ProfileGenerator.getImageData(props.tokenEntry));
-let progress = ref(0.6);
+const displayTokenText = ref(generator.hiddenTokenText);
+const progress = ref(0.6);
+
+//Hide/Show Button
+const hideButtonStyle = ref(true);
+function handleHideButton() {
+    hideButtonStyle.value = !hideButtonStyle.value;
+    if (hideButtonStyle.value) displayTokenText.value = generator.hiddenTokenText;
+    else displayTokenText.value = tokenText.value;
+}
+
+function handleEditButton() {
+    Dialog.create({
+        title: '<b style="color: #e63946">Not implemented</b>',
+        message: 'The functionality to edit individual tokens is not currently implemented in this version.',
+        cancel: false,
+        persistent: false,
+        html: true
+    });
+}
+
+const generatorsCallback: GeneratorsCallback = new class implements GeneratorsCallback {
+    updateToken(currentMillis: number) {
+        tokenText.value = generator.generateToken(currentMillis);
+        if (!hideButtonStyle.value) displayTokenText.value = tokenText.value;
+    }
+
+}
+
+let period: number | null = props.tokenEntry.secret.period;
+if (period !== null) {
+    TokenGenerators.addComponent(period, generatorsCallback);
+}
 </script>
 
 <template>
-<div class="tokenDiv">
-    <q-linear-progress :value="progress" class="q-mt-md progressBar" />
-    <img class="icon" :src="imageSrc" alt="Profile Image"/>
-    <div class="textDiv">
-        <p class="title">{{tokenEntry.issuer}}</p>
-        <p class="token">987 456</p>
+<div class="main-container">
+    <div class="token-container">
+        <q-linear-progress :value="progress" class="q-mt-md progressBar" />
+        <img class="profile-icon" :src="imageSrc" alt="Profile Image"/>
+        <div class="text-container">
+            <div class="title-container">
+                <p class="title">{{displayNames[0]}}</p>
+                <p class="title-second">{{displayNames.length === 2 ? displayNames[1] : ""}}</p>
+            </div>
+            <p class="token">{{ displayTokenText }}</p>
+        </div>
+    </div>
+    <div class="options-container">
+        <i :class="hideButtonStyle ? 'fa-solid fa-eye-slash options-icon' : 'fa-solid fa-eye options-icon'"
+           @click="handleHideButton"
+           :style="hideButtonStyle ? 'color: #1F4528;' : 'color: #9d0208;'">
+            <q-tooltip class="text-body2" :delay="350" :offset="[10, 10]">
+                {{ hideButtonStyle ? "Show token" : "Hide token"}}
+            </q-tooltip>
+        </i>
+        <i class="fa-solid fa-pen-to-square options-icon"
+        @click="handleEditButton">
+            <q-tooltip class="text-body2" :delay="350" :offset="[10, 10]">Edit</q-tooltip>
+        </i>
     </div>
 </div>
 </template>
 
 <style scoped>
-.tokenDiv {
-    width: 100vw;
+.main-container {
+    width: 97.2vw;
     height: 100px;
     max-height: 100px;
 
     display: flex;
+    justify-content: space-between;
+}
+
+.token-container {
+    display: flex;
     align-items: center;
 }
 
-.textDiv {
+.text-container {
     padding-left: 1vw;
 }
 
-.icon {
+.profile-icon {
     width: 65px;
     height: 65px;
     margin-left: 0.5vw;
@@ -55,11 +117,38 @@ p {
     padding: 0;
 }
 
-.title {
-    font-weight: bold;
+.token {
+    color: #dee2e6;
 }
 
-.token {
+.title-container {
+    display: flex;
+}
+
+.title {
+    font-weight: 500;
+}
+
+.title-second {
+    padding-left: 0.25rem;
     color: #adb5bd;
+}
+
+.options-container {
+    padding-right: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.options-icon {
+    font-size: 32px;
+    color: #495057;
+    padding-left: 10px;
+}
+
+.options-icon:hover {
+    color: #0353a4 !important;
+    cursor: pointer;
 }
 </style>
